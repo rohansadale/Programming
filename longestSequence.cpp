bdfa10fac3d01f8eb67489fcf163b52e5b423b60
Rohan Sadale <rohansadale@gmail.com>
2016-01-25 17:24:35 -0600

    Create longestSequence.cpp

---
+++ longestSequence.cpp
@@ -0,0 +1,22 @@
+// Longest non-decreasing sequence in array
+
+
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+main()
+{
+    vector<int>data = {5, 3, 4, 8, 6, 7};
+    vector<int>seq(6,1);
+    
+    for(int i = 1; i < data.size(); i++){
+        for(int j = 0; j < i; j++){
+            if(data[j] <= data[i] && seq[j] + 1 > seq[i] )
+                seq[i] = seq[j] + 1;
+        }
+        cout << data[i] << " " << seq[i]<< endl;
+    }
+    cout << *max_element(seq.begin(), seq.end());
+}
